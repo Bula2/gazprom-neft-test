6409021cb5d66f0ce5df9bf1a4bdd8ea34ebd6fe
Bula2 <bulaev123da@gmail.com>
2024-01-20 16:53:19 +0300

    updates

--- README.md
+++ README.md
@@ -1,46 +1,22 @@
-# Getting Started with Create React App
+Привет, тебя приветствует команда Управления данными компании [Газпромнефть-ЦР](https://ds.gazprom-neft.ru/)!
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+В рамках данного тестового задания тебе необходимо будет реализовать небольшое React-приложение с использованием библиотеки [echarts.js](https://echarts.apache.org/en/index.html) и библиотеки компонентов [Consta UI Kit](https://consta.design/libs/uikit)
 
-## Available Scripts
+* Макет дешборда ты можешь увидеть здесь: [Figma]()
+  
+* Пример того, как должна выглядеть готовая реализация вот здесь: [Видео]()
 
-In the project directory, you can run:
+* В папке проекта создан шаблон React-приложения, который можно использовать для работы.
 
-### `npm start`
+* В файл [package.json](./package.json) описаны минимально-необходимыми для работы библиотеки.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+* В файле [ReactECharts.ts](./src/Echarts/ReactECharts.tsx) реализован компонент ReactECharts, который можно использовать для отрисовки элементов из библиотеки [echarts.js](https://echarts.apache.org/en/index.html).
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+Пример использования:
+```
+<ReactECharts option={option}/> 
+```
 
-### `npm test`
+* В файле [data.ts]() представлен массив данных, который необходимо использовать для отрисовки визуализации в рамках разработки. Будет преимуществом реализации запроса к данным по api. Для этого вы можете использовать библиотеку [axios] или стандартный fetch. В качестве эндпоинта вы можете использовать сервис [mockapi] или любой другой аналогичный сервис, куда необходимо будет внести данные, представленные в файле  [data.ts]()
+1. 
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,332 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Text } from '@consta/uikit/Text';
 import { ReactECharts } from './Echarts/ReactECharts';
+import { presetGpnDefault, Theme } from '@consta/uikit/Theme';
+import { ChoiceGroup } from '@consta/uikit/ChoiceGroup';
+import { Layout } from '@consta/uikit/Layout';
+
+const data = [
+  {
+    date: '2016-02-01',
+    month: 'фев 2016',
+    indicator: 'Курс доллара',
+    value: 72,
+  },
+  {
+    date: '2016-03-02',
+    month: 'мар 2016',
+    indicator: 'Курс доллара',
+    value: 80,
+  },
+  {
+    date: '2016-04-01',
+    month: 'апр 2016',
+    indicator: 'Курс доллара',
+    value: 77,
+  },
+  {
+    date: '2016-05-02',
+    month: 'май 2016',
+    indicator: 'Курс доллара',
+    value: 78,
+  },
+  {
+    date: '2016-02-01',
+    month: 'июн 2016',
+    indicator: 'Курс доллара',
+    value: 77,
+  },
+  {
+    date: '2016-03-02',
+    month: 'июл 2016',
+    indicator: 'Курс доллара',
+    value: 76,
+  },
+  {
+    date: '2016-04-01',
+    month: 'авг 2016',
+    indicator: 'Курс доллара',
+    value: 81,
+  },
+  {
+    date: '2016-05-02',
+    month: 'сент 2016',
+    indicator: 'Курс доллара',
+    value: 82,
+  },
+
+  {
+    date: '2016-02-01',
+    month: 'фев 2016',
+    indicator: 'Курс евро',
+    value: 90,
+  },
+  {
+    date: '2016-03-02',
+    month: 'мар 2016',
+    indicator: 'Курс евро',
+    value: 88,
+  },
+  {
+    date: '2016-04-01',
+    month: 'апр 2016',
+    indicator: 'Курс евро',
+    value: 87,
+  },
+  {
+    date: '2016-05-02',
+    month: 'май 2016',
+    indicator: 'Курс евро',
+    value: 91,
+  },
+  {
+    date: '2016-02-01',
+    month: 'июн 2016',
+    indicator: 'Курс евро',
+    value: 92,
+  },
+  {
+    date: '2016-03-02',
+    month: 'июл 2016',
+    indicator: 'Курс евро',
+    value: 93,
+  },
+  {
+    date: '2016-04-01',
+    month: 'авг 2016',
+    indicator: 'Курс евро',
+    value: 89,
+  },
+  {
+    date: '2016-05-02',
+    month: 'сент 2016',
+    indicator: 'Курс евро',
+    value: 88,
+  },
+
+  {
+    date: '2016-02-01',
+    month: 'фев 2016',
+    indicator: 'Курс юаня',
+    value: 22,
+  },
+  {
+    date: '2016-03-02',
+    month: 'мар 2016',
+    indicator: 'Курс юаня',
+    value: 24,
+  },
+  {
+    date: '2016-04-01',
+    month: 'апр 2016',
+    indicator: 'Курс юаня',
+    value: 25,
+  },
+  {
+    date: '2016-05-02',
+    month: 'май 2016',
+    indicator: 'Курс юаня',
+    value: 21,
+  },
+  {
+    date: '2016-02-01',
+    month: 'июн 2016',
+    indicator: 'Курс юаня',
+    value: 23,
+  },
+  {
+    date: '2016-03-02',
+    month: 'июл 2016',
+    indicator: 'Курс юаня',
+    value: 24,
+  },
+  {
+    date: '2016-04-01',
+    month: 'авг 2016',
+    indicator: 'Курс юаня',
+    value: 26,
+  },
+  {
+    date: '2016-05-02',
+    month: 'сент 2016',
+    indicator: 'Курс юаня',
+    value: 19,
+  },
+];
+
+const values = [
+  {
+    symbol: '$',
+    name: 'Курс доллара',
+  },
+  {
+    symbol: '€',
+    name: 'Курс евро',
+  },
+  {
+    symbol: '¥',
+    name: 'Курс юаня',
+  },
+];
 
 function App() {
+  const [currency, setCurrency] = useState({
+    symbol: '$',
+    name: 'Курс доллара',
+  });
+
   const option = {
+    grid: {
+      top: 20,
+      left: 60,
+      right: '3%',
+      bottom: 36,
+    },
     tooltip: {
       trigger: 'axis',
       axisPointer: {
-        type: 'shadow'
-      }
-    },
-    legend: {
-      data: ['Profit', 'Expenses', 'Income']
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    xAxis: [
-      {
-        type: 'value'
-      }
-    ],
-    yAxis: [
-      {
-        type: 'category',
-        axisTick: {
-          show: false
-        },
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      }
-    ],
-    series: [
-      {
-        name: 'Profit',
-        type: 'bar',
-        label: {
-          show: true,
-          position: 'inside'
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [200, 170, 240, 244, 200, 220, 210]
+        type: 'line',
       },
-      {
-        name: 'Income',
-        type: 'bar',
-        stack: 'Total',
-        label: {
-          show: true
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [320, 302, 341, 374, 390, 450, 420]
+      confine: true,
+      formatter(params: { name: string; marker: string; value: number }[]) {
+        const series = params.reduce((result, item, index) => {
+          result += item.value
+            ? `<div class = tooltip__main>
+              <div class = tooltip__main-title>${item.marker} ${'qq'}</div>
+              <div class = tooltip__main-value>${item.value}</div>
+            </div>`
+            : '';
+          return result;
+        }, '');
+        return `
+          <div class = tooltip__card>
+            <div class = tooltip__title>
+              ${params[0].name} год
+            </div>
+            <div>
+              ${series}
+            </div>
+            <div class= tooltip__comments>
+            </div>
+        </div>
+          `;
       },
-      {
-        name: 'Expenses',
-        type: 'bar',
-        stack: 'Total',
-        label: {
-          show: true,
-          position: 'left'
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: data
+        .filter((item) => item.indicator === currency.name)
+        .map((item) => item.month),
+      axisLine: {
+        show: true,
+        lineStyle: {
+          type: 'dotted',
+          color: '#ccd9e0',
         },
-        emphasis: {
-          focus: 'series'
+      },
+      axisTick: {
+        show: false,
+      },
+      splitLine: {
+        show: false,
+        lineStyle: {
+          type: 'dotted',
+          color: '#ccd9e0',
         },
-        data: [-120, -132, -101, -134, -190, -230, -210]
-      }
-    ]
+      },
+      axisLabel: {
+        show: true,
+        color: '#667985',
+        fontSize: 12,
+        fontFamily: 'Inter',
+      },
+    },
+    yAxis: {
+      type: 'value',
+      splitNumber: 3,
+      scale: true,
+      splitLine: {
+        show: true,
+        lineStyle: {
+          type: 'dotted',
+          color: '#ccd9e0',
+        },
+      },
+      axisLine: {
+        lineStyle: {
+          type: 'dotted',
+          color: '#ccd9e0',
+        },
+      },
+      axisLabel: {
+        color: '#667985',
+        fontSize: 12,
+        fontFamily: 'Inter',
+        showMinLabel: false,
+        margin: 16,
+      },
+    },
+    series: {
+      name: 'График',
+      data: data
+        .filter((item) => item.indicator === currency.name)
+        .map((item) => item.value),
+      type: 'line',
+      lineStyle: {
+        color: 'red',
+      },
+      symbol: 'circle',
+      label: {
+        show: false,
+        position: 'top',
+        formatter: (params: { value: number }) => params.value?.toFixed(0),
+      },
+      barMaxWidth: 20,
+    },
   };
-  
-  
+
+  console.log(currency);
+
   return (
-    <div className="App">
-      <ReactECharts option={option}/> 
-    </div>
+    <Theme preset={presetGpnDefault}>
+      <div className="App">
+        <Layout
+          style={{
+            width: '1200px',
+            height: '500px',
+            backgroundColor: 'white',
+            borderRadius: '8px',
+            padding: '8px',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Layout
+            style={{
+              width: '100%',
+              height: '25px',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Text weight="bold" size="xl">
+              {currency.name} к рублю
+            </Text>
+            <ChoiceGroup
+              size="xs"
+              value={currency}
+              onChange={({ value }) => setCurrency(value)}
+              items={values}
+              getItemLabel={(item) => item.symbol}
+              multiple={false}
+            />
+          </Layout>
+          <ReactECharts option={option} />
+        </Layout>
+      </div>
+    </Theme>
   );
 }
 
